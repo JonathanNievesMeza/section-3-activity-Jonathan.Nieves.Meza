998679129338e6af24e28a614db0a778ef24a1fa
Jonathan Nieves <jonathan.nieves@e3civichigh.com>
2019-02-22 10:42:43 -0800

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new scanner(System.in);
+
+        System.out.print("What operation? (+, -, *. /");
+        String operation = scanner.next();
+
+        System.out.print("What is the first number? ");
+        double firstNumber = scanner.nextDouble();
+
+        System.out.print("What is the second number? ");
+        double secondNumber = scanner.nextDouble();
+
+        
+    }
+}
